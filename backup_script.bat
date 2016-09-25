7a8a9021990a9daa08438bdba9282cd29a5f2b28
Ashley <ashley.m.manson@gmail.com>
2016-09-25 12:14:06 +1000

    add comments

--- backup_script.bat
+++ backup_script.bat
@@ -1,5 +1,15 @@
+::
 :: A Windows backup script
-:: Ashley Manson
+:: Who made this: Ashley Manson
+::
+:: Backup all specified directories in ".backup_directories".
+:: Paths with spaces need to be surrounded with double quotes.
+:: If a paths last directory has a space, it will fail to backup.
+::
+:: First parameter should be a drive letter eg E: or G:
+:: Second parameter is optional, "no-prompt" will backup without prompting user,
+:: useful if running as a shortcut or as a scheduled task.
+::
 
 @echo off
 
@@ -9,11 +19,10 @@ cls
 
 :: First parameter should be the backup drive letter.
 set BACKUP_DRIVE=%1
+set PROMPT_USER=%2
 
 set BACKUP_PATH=%BACKUP_DRIVE%\Backup\%USERNAME%
 set LOG_PATH=%BACKUP_DRIVE%\Backup\.logs
-
-set PROMPT_USER=%2
 
 if "%PROMPT_USER%" == "no-prompt" (
 	goto START
@@ -25,17 +34,18 @@ if /i "%SHOULD_CONTINUE%" NEQ "Y" goto :EOF
 
 :START
 
-:: Ensure all directories exist before continuing 
+:: Ensure all directories exist before continuing.
 if not exist %BACKUP_PATH% mkdir %BACKUP_PATH%
 if not exist %LOG_PATH% mkdir %LOG_PATH%
 
-:: Format date and time for log file name, and create empty log file
+:: Format date and time for log file name, and create empty log file.
 for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set the_date=%%c-%%b-%%a)
 for /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set the_time=%%a%%b)
 set the_time=%the_time: =0%
 set LOG_FILE="%LOG_PATH%\%the_date%_%the_time%_backup.log"
 echo. 2> %LOG_FILE%
 
+:: Do the backup now.
 for /f "tokens=*" %%A in (.backup_directories) do (
     title Backup Script [%%A to %BACKUP_PATH%\%%~nxA]
     robocopy %%A %BACKUP_PATH%\%%~nxA /e /xjd /eta /tee /mt:4 /log+:%LOG_FILE%
@@ -48,4 +58,3 @@ if "%PROMPT_USER%" == "no-prompt" (
 )
 
 pause
-
